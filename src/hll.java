d490c9208bd66b901d664c441c38462af7bf41e2
gwyhxl <gwyhxl1991@163.com>
2021-07-20 23:06:07 +0800

    22222

--- src/hll.java
+++ src/hll.java
@@ -1,6 +1,6 @@
 public class hll {
     public static void main(String[] args) {
 
-        System.out.println("HHHH11111555111551HHHHH");
+        System.out.println("HHHH1111111551HHHHH");
     }
 }
